b9247ac9e137b3d707296c89f9ef264cb30a4ce0
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2022-12-13 11:04:35 -0700

    Refactored site header to mui navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,17 +2,15 @@ import React, { ReactNode } from "react";
 import Link from "next/link";
 import Head from "next/head";
 
+import NavBar from "./NavBar";
+
 type Props = {
 	children?: ReactNode;
 	userData?: object;
 	title?: string;
 };
 
-const Layout = ({
-	children,
-	userData = null,
-	title = "This is the default title",
-}: Props) => {
+const Layout = ({ children, title = "This is the default title" }: Props) => {
 	return (
 		<div>
 			<Head>
@@ -20,13 +18,7 @@ const Layout = ({
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
-			<header>
-				<nav>
-					<Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
-					<Link href="/users">Recipe History</Link> |{" "}
-					<Link href="/login">{userData ? "Sign out" : "Login"}</Link>
-				</nav>
-			</header>
+			<NavBar />
 			{children}
 			<footer>
 				<hr />
---
+++ components/NavBar.tsx
@@ -0,0 +1,153 @@
+import { useState, MouseEvent } from "react";
+
+import Link from "next/link";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+
+const BRAND_NAME = "WSIET";
+
+const styles = {
+	menuLink: {
+		marginRight: "12px",
+		color: "white",
+		textTransform: "uppercase",
+		textDecoration: "none",
+		fontWeight: "bold",
+		letterSpacing: "2px",
+	},
+};
+
+function NavBar() {
+	const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+	const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+		setAnchorElNav(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	return (
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					{/* Brand */}
+					<AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+					<Link href="/" passHref style={{ textDecoration: "none" }}>
+						<Typography
+							variant="h6"
+							component="p"
+							noWrap
+							sx={{
+								mr: 2,
+								display: { xs: "none", md: "flex" },
+								fontFamily: "monospace",
+								fontWeight: 700,
+								letterSpacing: ".3rem",
+								color: "white",
+								textDecoration: "none",
+							}}
+						>
+							{BRAND_NAME}
+						</Typography>
+					</Link>
+					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<IconButton
+							size="large"
+							aria-label="menu items"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit"
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}
+						>
+							<MenuItem sx={{ mr: 2 }}>
+								<Link href="/about">About</Link>
+							</MenuItem>
+							<MenuItem onClick={handleCloseNavMenu}>
+								<Link href="/recipe-history">Recipe History</Link>
+							</MenuItem>
+						</Menu>
+					</Box>
+					<AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+					<Typography
+						variant="h5"
+						noWrap
+						component="a"
+						href="/"
+						sx={{
+							mr: 2,
+							display: { xs: "flex", md: "none" },
+							flexGrow: 1,
+							fontFamily: "monospace",
+							fontWeight: 700,
+							letterSpacing: ".3rem",
+							color: "inherit",
+							textDecoration: "none",
+						}}
+					>
+						{BRAND_NAME}
+					</Typography>
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						<Link
+							href="/about"
+							style={{
+								marginRight: "12px",
+								color: "white",
+								textTransform: "uppercase",
+								textDecoration: "none",
+								fontWeight: "bold",
+								letterSpacing: "2px",
+							}}
+						>
+							About
+						</Link>
+						<Link
+							href="/recipe-history"
+							style={{
+								marginRight: "12px",
+								color: "white",
+								textTransform: "uppercase",
+								textDecoration: "none",
+								fontWeight: "bold",
+								letterSpacing: "2px",
+							}}
+						>
+							Recipe History
+						</Link>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
+}
+export default NavBar;
