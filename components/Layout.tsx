d0eabc0f7c4be903876085a802bd7ec67dbc9c2d
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2022-12-13 08:57:20 -0700

    Updated navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,30 +4,37 @@ import Head from "next/head";
 
 type Props = {
 	children?: ReactNode;
+	userData?: object;
 	title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
-	<div>
-		<Head>
-			<title style={{ textTransform: "capitalize" }}>{title}</title>
-			<meta charSet="utf-8" />
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-		</Head>
-		<header>
-			<nav>
-				<Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
-				<Link href="/users">Users List</Link> |{" "}
-				<a href="/api/users">Users API</a>
-			</nav>
-		</header>
-		{children}
-		<footer>
-			<hr />
-			Made with &hearts; by{" "}
-			<a href="https://alchemizedsoftware.com">Alchemized Software</a>
-		</footer>
-	</div>
-);
+const Layout = ({
+	children,
+	userData = null,
+	title = "This is the default title",
+}: Props) => {
+	return (
+		<div>
+			<Head>
+				<title style={{ textTransform: "capitalize" }}>{title}</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<header>
+				<nav>
+					<Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
+					<Link href="/users">Recipe History</Link> |{" "}
+					<Link href="/login">{userData ? "Sign out" : "Login"}</Link>
+				</nav>
+			</header>
+			{children}
+			<footer>
+				<hr />
+				Made with &hearts; by{" "}
+				<a href="https://alchemizedsoftware.com">Alchemized Software</a>
+			</footer>
+		</div>
+	);
+};
 
 export default Layout;
