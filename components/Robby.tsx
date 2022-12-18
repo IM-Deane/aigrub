aa0b96809ffdd72c6129d34e025d75298f69da26
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2022-12-17 20:41:30 -0700

    Robby is not clickable so mobile devices can see him speak

--- components/Robby.tsx
+++ components/Robby.tsx
@@ -1,11 +1,32 @@
+import * as React from "react";
+
+import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 
 import SmartToyIcon from "@mui/icons-material/SmartToy";
 
 export default function Robby({ title, placement, fontSize }) {
+	const [open, setOpen] = React.useState(false);
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
 	return (
-		<Tooltip title={title} placement={placement}>
-			<SmartToyIcon fontSize={fontSize || "medium"} />
+		<Tooltip
+			open={open}
+			onClose={handleClose}
+			title={title}
+			placement={placement}
+			arrow
+		>
+			<IconButton aria-label="Robby" onClick={handleOpen}>
+				<SmartToyIcon fontSize={fontSize || "medium"} />
+			</IconButton>
 		</Tooltip>
 	);
 }
