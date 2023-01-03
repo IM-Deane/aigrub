ede0557e44ce8eaf26145b717932c214b9eafede
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-01-02 20:17:12 -0700

    Added additional tests

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../pages/index";
+import Home from "../../pages/index";
 import "@testing-library/jest-dom";
-import { getRandomListOfKeywords } from "../utils";
+import { getRandomListOfKeywords } from "../../utils";
 
 describe("Home", () => {
 	const defaultProps = {
 		initialRandomKeywords: getRandomListOfKeywords(),
 	};
 
-	it("renders the home page", () => {
+	it("page renders correctly", () => {
 		render(<Home {...defaultProps} />);
 
 		const heading = screen.getByRole("heading", {
